495d4d37b2efd53aee1a6e64e3cb96132f7c3514
Ryan Scott <ryan.matt.scott@gmail.com>
2018-06-04 23:22:47 -0400

    edit docstring

--- media.py
+++ media.py
@@ -2,7 +2,12 @@ import webbrowser
 
 
 class Movie(object):
-    """docstring for ."""
+    """The Movie class instantiates movie objects using the movie title, poster
+    image url, and youtube trailer url as strings
+    args:
+        title        (str): Movie title
+        img_link     (str): Movie Poster Image URL
+        trailer_link (str): Movie Trailer YouTube URL """
     def __init__(self, title, img_link, trailer_link):
         self.title = title
         self.poster_image_url = img_link
